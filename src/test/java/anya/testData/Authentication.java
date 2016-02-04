ad1a56da1e4457dd5b28095b8f57e475f64b109d
anna_goncharova <anna@bandit-ltd.com>
2016-02-04 18:18:02 +0200

    small change

--- src/test/java/anya/testData/Authentication.java
+++
@@ -1,6 +0,0 @@
-package anya.testData;
-
-public class Authentication {
-    public static String userName = "annabandit.fortest@gmail.com";
-    public static String password = "annabanditfortest";
-}
